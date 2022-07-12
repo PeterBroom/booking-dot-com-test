bee3741f8ec9321e5d9e2813fb142cbf045366e8
PeterBroom <harleybroom@googlemail.com>
2022-07-12 16:56:56 +0100

    wrap the app with search provider from context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import SearchProvider from '@/context/SearchContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SearchProvider>
+      <Component {...pageProps} />
+    </SearchProvider>
+  )
 }
 
 export default MyApp
