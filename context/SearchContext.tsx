637f639567246f070a3565c7829fd2f571908cc6
PeterBroom <harleybroom@googlemail.com>
2022-07-12 22:17:07 +0100

    tidy up and format

--- context/SearchContext.tsx
+++ context/SearchContext.tsx
@@ -1,41 +1,28 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
 const SearchContext = createContext<any>(undefined);
 
 export default function SearchProvider({ children }: any) {
-    const [searchResults, setSearchResults] = useState(null)
+  const [searchResults, setSearchResults] = useState(null)
 
-    async function getSearchResults(location: string){
-      console.log('api param',location)
-        axios.post("/api/search/" + location).then((res) => {
-            setSearchResults(res.data.docs)
-        })
-    }
-
-    async function autoCompleteResults(location: string){
-      axios.post("/api/search").then((res) => {
-        setSearchResults(res.data.docs)
-      })
-    }
-
-//   useEffect(() => {
-
-//   }, []);
-// async function getResults(newItem: any) {
-// }
-
+  async function getSearchResults(location: string) {
+    console.log('api param', location)
+    axios.post("/api/search/" + location).then((res) => {
+      setSearchResults(res.data.docs)
+    })
+  }
 
   return (
     <SearchContext.Provider
       value={{
-        searchResults, 
+        searchResults,
         getSearchResults
       }}
     >
       {children}
     </SearchContext.Provider>
-    );
+  );
 }
 
 const SearchConsumer = SearchContext.Consumer;
