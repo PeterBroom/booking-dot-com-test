58454088dc328643b5c42d4e084297d475f925e5
PeterBroom <harleybroom@googlemail.com>
2022-07-12 22:29:40 +0100

    add setSearchResults to SearchContext

--- context/SearchContext.tsx
+++ context/SearchContext.tsx
@@ -17,6 +17,7 @@ export default function SearchProvider({ children }: any) {
     <SearchContext.Provider
       value={{
         searchResults,
+        setSearchResults,
         getSearchResults
       }}
     >
